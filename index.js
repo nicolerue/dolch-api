147cda431c711e24e28660680261da2f89f648af
Nicole Rue <nicolerue92@gmail.com>
2023-10-25 17:07:49 -0700

    add data to routes

--- index.js
+++ index.js
@@ -1,18 +1,42 @@
 const express = require("express");
-
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const port = process.env.PORT || 3001;
 const app = express();
-const PORT = 4000;
+const { grade1Words } = require("./grade1Words.js");
+const { grade2Words } = require("./grade2Words.js");
+const { grade3Words } = require("./grade3Words.js");
 
-app.listen(PORT, () => {
-  console.log(`API listening on PORT ${PORT} `);
-});
+app.locals = {
+  title: "dolch-words",
+  grade1Words,
+  grade2Words,
+  grade3Words,
+};
+
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
-  res.send("Hey this is my API running 🥳");
+  res.status(200).json("hello");
 });
 
-app.get("/about", (req, res) => {
-  res.send("This is my about route..... ");
+app.get("/api/v1/grade1", (req, res) => {
+  res.status(200).json(app.locals.grade1Words);
 });
 
+app.get("/api/v1/grade2", (req, res) => {
+  res.status(200).json(app.locals.grade2Words);
+});
+
+app.get("/api/v1/grade3", (req, res) => {
+  res.status(200).json(app.locals.grade3Words);
+});
+
+app.listen(port, () => {
+  console.log(
+    `${app.locals.title} is now running on http://localhost:${port} !`
+  );
+});
 module.exports = app;
